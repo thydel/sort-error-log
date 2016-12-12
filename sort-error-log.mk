21c169cfbc00fa306e240c18f496d60c9e79b52b
Thierry Delamare <t.delamare@epiconcept.fr>
2016-12-12 20:28:52 +0100

    Add sorted aggregate by node

--- sort-error-log.mk
+++ sort-error-log.mk
@@ -21,9 +21,16 @@ txt     = $(node)-%.txt: $(dir)/$1-error.d/%.log;
 txt    += < $$< sed -re '$($1)' | sort | uniq -c | sort -nr > $$@
 .      := $(foreach _, $(set), $(eval $(call txt,$_)))
 
-main: $(set)
+main: $(node).txt
 $(set): $(txts)
 .PHONY: main $(set) $(txts)
+
+ls = ls -lSsh $1
+wc = wc $1/* | sort -nrk3
+showdir  = test -d $1 && (echo $(ls); $(ls); echo; echo '$(strip $(wc))'; $(wc); echo);
+$(node).txt  = ($(foreach _, $(set), $(call showdir, $(dir)/$_-error.d))) > $@;
+$(node).txt += grep . $^ | sort -k2 -nr | sed -e s/.txt:/$$'\1'/ | column -ts$$'\1' >> $@
+$(node).txt: $(foreach _,$(txts),$($_)); $($@)
 
 .SECONDEXPANSION:
 $(txts): $$($$@)
