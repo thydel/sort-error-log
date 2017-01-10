f01d6b7b8380477a0d5f43d5655bbdb626eff952
Thierry Delamare <thy@debian8.admin2.oxa.tld>
2017-01-10 18:46:45 +0100

    sync

--- bar_chart_lines.mk
+++ bar_chart_lines.mk
@@ -1,4 +1,8 @@
+#!/usr/bin/make -f
+
 top:; @date
+
+bar_chart_lines.mk:;
 
 today     := $(shell date +%Y/%m/%d)
 yesterday := $(shell date -d yesterday +%Y/%m/%d)
@@ -13,17 +17,25 @@ $(http_access) := _ssl_access.log
 $(http_error)  := _error.log
 $(php_error)   := _php_errors.log
 
+base := /space/remote_logs
+
 define 00-lines.txt
 (
-  cd $(@D);
-  find -type f -name '*$1' -print0
-  | xargs -0i wc -l {}
-  | sed -e s/$1// -e 's/\.\///'
-  | bar_chart.py -avrp --dot .
-  > $(@F)
+  test -d $(@D) && (
+    cd $(@D);
+    find -type f -name '*$1' -print0
+    | xargs -0i wc -l {}
+    | sed -e s/$1// -e 's/\.\///'
+    | bar_chart.py -avrp --dot .
+    > $(@F)
+  ) || (
+    mkdir -p $(@D);
+    touch $(@F);
+  )
 )
 endef
 
+/etc/sort-error-log/fronts.mk:;
 include /etc/sort-error-log/fronts.mk
 
 date  ?= yesterday
@@ -31,6 +43,7 @@ nodes ?= $(fronts)
 logs  ?= http_access http_error php_error
 files := $(foreach node, $(nodes), $(foreach log, $(logs), $(node)/$($(date))/$($(log))/00-lines.txt))
 
-$(files):; $(strip $(call $(@F),$($(notdir $(@D)))))
-files: $(files)
+#$(warning $(files:%=$(base)/%))
+$(files:%=$(base)/%):; $(strip $(call $(@F),$($(notdir $(@D)))))
+files: $(files:%=$(base)/%)
 .PHONY: files
